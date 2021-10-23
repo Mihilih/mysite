5a8f65cd738e0b2f7519ba351f65cf45835d76ea
unknown <mihilih@gmail.com>
2021-10-23 16:15:58 +0530

    createsuperuser

--- mysite/cerps/admin.py
+++ mysite/cerps/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Jounral)
+admin.site.register(Book)
+admin.site.register(Patent)
+admin.site.register(Grant)
+admin.site.register(Award)
