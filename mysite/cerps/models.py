cc6a34f42285648d25f53f8dd7a7586a7f367cbc
unknown <mihilih@gmail.com>
2021-10-23 16:31:27 +0530

    models

--- mysite/cerps/models.py
+++ mysite/cerps/models.py
@@ -10,7 +10,9 @@ class User(AbstractUser):
 class Jounral(models.Model):
     id=models.AutoField(primary_key=True)
     #name
+    name=models.TextField(max_length=10000, blank=False, default="")
     #title
+    title=models.TextField(max_length=10000, blank=False, default="")
     #authors
     #volume
     #issue
@@ -19,26 +21,35 @@ class Jounral(models.Model):
 class Book(models.Model):
     id=models.AutoField(primary_key=True)
     #book/book chapter
+    book_chap=models.TextField(max_length=10000, blank=False, default="")
     #book title
+    book_title=models.TextField(max_length=10000, blank=False, default="")
     #chapter title
+    chap_title=models.TextField(max_length=10000, blank=False, default="")
     #authors
     #year
     #publisher
+    isbn=models.TextField(max_length=10000, blank=False, default="")
     #ISBN
 
 class Patent(models.Model):
     id=models.AutoField(primary_key=True)
     #filing agency
+    agency=models.TextField(max_length=10000, blank=False, default="")
     #title
+    title=models.TextField(max_length=10000, blank=False, default="")
     #number
-    #authours
+    #authors
     #year
 
 class Grant(models.Model):
     id=models.AutoField(primary_key=True)
     #funding agency
+    agency=models.TextField(max_length=10000, blank=False, default="")
     #title
-    #name if PI
+    title=models.TextField(max_length=10000, blank=False, default="")
+    #name of PI
+    pi=models.TextField(max_length=10000, blank=False, default="")
     #co-inversigators
     #budgets
     #duration
@@ -46,6 +57,9 @@ class Grant(models.Model):
 class Award(models.Model):
     id=models.AutoField(primary_key=True)
     #name
+    name=models.TextField(max_length=10000, blank=False, default="")
     #award
+    award=models.TextField(max_length=10000, blank=False, default="")
     #awarding agency
+    agency=models.TextField(max_length=10000, blank=False, default="")
     #year
