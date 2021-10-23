f9bf095b5985a658aa22b13db91d8939c3875646
unknown <mihilih@gmail.com>
2021-10-23 16:12:50 +0530

    models

--- mysite/cerps/models.py
+++ mysite/cerps/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class User(AbstractUser):
+    id=models.AutoField(primary_key=True)
+    pass
+
+class Jounral(models.Model):
+    id=models.AutoField(primary_key=True)
+    #name
+    #title
+    #authors
+    #volume
+    #issue
+    #pages
+
+class Book(models.Model):
+    id=models.AutoField(primary_key=True)
+    #book/book chapter
+    #book title
+    #chapter title
+    #authors
+    #year
+    #publisher
+    #ISBN
+
+class Patent(models.Model):
+    id=models.AutoField(primary_key=True)
+    #filing agency
+    #title
+    #number
+    #authours
+    #year
+
+class Grant(models.Model):
+    id=models.AutoField(primary_key=True)
+    #funding agency
+    #title
+    #name if PI
+    #co-inversigators
+    #budgets
+    #duration
+
+class Award(models.Model):
+    id=models.AutoField(primary_key=True)
+    #name
+    #award
+    #awarding agency
+    #year
--- mysite/cerps/views.py
+++ mysite/cerps/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "cerps/index.html")
-
-def downloads(request):
-    return render(request, "cerps/downloads.html")
