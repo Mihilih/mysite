7eff0ed5f11ecf79a9c124ca36a5a284b7c94c13
unknown <mihilih@gmail.com>
2021-10-24 18:35:02 +0530

    migrations

--- mysite/cerps/admin.py
+++ mysite/cerps/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(People)
 admin.site.register(Jounral)
 admin.site.register(Book)
 admin.site.register(Patent)
