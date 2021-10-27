4d35eecfa760ce3d57590b5aa4ee71b0ebeadbbe
unknown <mihilih@gmail.com>
2021-10-27 17:31:05 +0530

    data files

--- mysite/cerps/views.py
+++ mysite/cerps/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "cerps/index.html")
+
+def add(request):
+    return render(request, "cerps/add.html")
