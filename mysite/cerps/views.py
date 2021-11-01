c6d0fe731e3c47597bb95de79f75735e557197c3
unknown <mihilih@gmail.com>
2021-11-01 07:10:38 +0530

    add data

--- mysite/cerps/views.py
+++ mysite/cerps/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from cerps.models import *
 
 # Create your views here.
 def index(request):
@@ -10,7 +19,35 @@ def add(request):
 #add and display journal
 def journal(request):
     if request.method == "PUT":
-        pass
+        name=request.POST['name']
+        title=request.POST['title']
+        authors=request.POST['authors']
+        authlist=[x.strip() for x in authors.split(',')]
+        auth=[]
+        for x in authlist:
+            try:
+                person=People.objects.get(name=x)
+                auth.append[person]
+            except People.DoesNotExist:
+                person=People()
+                person.name=x;
+                person.save()
+                per=People.objects.get(name=x)
+                auth.append[person]
+        volume=request.POST['volume']
+        issue=request.POST['issue']
+        pages=request.POST['pages']
+        jounal=Jounral()
+        journal.name=name
+        journal.title=title
+        journal.volume=volume
+        journal.issue=issue
+        journal.pages=pages
+        journal.save()
+        for x in auth:
+            jounal.authors.add(x)
+        journal.save()
+        return render(request, "cerps/journaladd.html")
     else:
         return render(request, "cerps/journaladd.html")
 
